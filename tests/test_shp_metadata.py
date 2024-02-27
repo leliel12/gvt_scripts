8f99b9add2378794d612a827d5648154e9b96411
juanbc <jbcabral@unc.edu.ar>
2024-02-27 11:10:15 -0300

    remove tests

--- tests/test_shp_metadata.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# License: BSD-3 (https://tldrlegal.com/license/bsd-3-clause-license-(revised))
-# Copyright (c) 2024, JB Cabral, GVT-CONAE
-# All rights reserved.
